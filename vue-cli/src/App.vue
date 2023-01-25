2c49df7be68ed19afee12dfa88af137386671b44
payo03 <payo03@cnspartner.com>
2023-01-25 17:15:11 +0900

    230125 Vue.js 강의본

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     {{ str }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      str: 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- HTML -->
+    <div>header</div>
+</template>
+
+<script>
+// Javascript
+export default {
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
