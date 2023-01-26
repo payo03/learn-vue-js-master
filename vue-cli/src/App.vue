85e8570645db51334b2cb8528e76e085e845b373
payo03 <payo03@cnspartner.com>
2023-01-26 15:43:16 +0900

    23-01-26 Vue.js 강의

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
-     {{ str }}
+     <app-header 
+        v-bind:propsdata="str"
+        v-on:renew="renewStr"
+        v-on:back="rollbackStr">
+     </app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
   data: function() {
     return {
-      str: 'hi'
+      str: 'Header',
+      outVar : 'hi'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = this.outVar;
+    },
+    rollbackStr: function() {
+      this.str = 'Header'
     }
   }
 }
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+    <button v-on:click="sendEvent">send</button>
+    <button v-on:click="rollback">back</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function() {
+            this.$emit('renew')
+        },
+        rollback: function() {
+            this.$emit('back')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
